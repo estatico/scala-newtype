b71c6085ec5e5db49958bfa0671b490beb48f034
Cary Robbins <carymrobbins@gmail.com>
2017-05-05 20:10:21 -0400

    Removed required type param from NewType

--- src/main/scala/io/estatico/newtype/NewType.scala
+++ src/main/scala/io/estatico/newtype/NewType.scala
@@ -2,44 +2,53 @@ package io.estatico.newtype
 
 import io.estatico.newtype.ops.NewTypeOps
 
-trait NewType[R] { self =>
+trait NewType {
   final type Type = NewType.Aux[Tag, Repr]
-  final type Repr = R
-  final abstract class Tag
+  type Repr
+  trait Tag
 }
 
 object NewType {
-  // The `with Tagged[T]` is a trick to force the compiler to look in the user's
-  // object which extends NewType[R] for implicits.
-  type Aux[T, R] = ({ type Tag = T ; type Repr = R }) with Tagged[T]
+
+  /**
+   * The `with Tagged[T]` is a trick to force the compiler to look in the user's
+   * object which extends NewType[R] for implicits.
+   */
+  type Aux[T, R] = ({ type Repr = R }) with Tagged[T]
   trait Tagged[T]
+
+  trait For[R] extends NewType {
+    final type Repr = R
+  }
+
+  trait Default[R] extends For[R] with NewTypeExtras
 }
 
-trait DefaultNewType[R]
-  extends NewTypeApplyM[R]
-  with NewTypeDeriving[R]
-  with NewTypeAutoOps[R]
-
-trait NewTypeAutoOps[R] extends NewType[R] {
+trait NewTypeAutoOps extends NewType {
   implicit def toNewTypeOps(x: Type): NewTypeOps[Tag, Repr] = new NewTypeOps[Tag, Repr](x)
 }
 
-trait NewTypeCasts[R] extends NewType[R] {
+trait NewTypeCasts extends NewType {
   @inline protected def cast(x: Repr): Type = x.asInstanceOf[Type]
   @inline protected def castM[M[_]](mx: M[Repr]): M[Type] = mx.asInstanceOf[M[Type]]
 }
 
-trait NewTypeApply[R] extends NewType[R] {
+trait NewTypeApply extends NewType {
   /** Convert a `Repr` to a `Type`. */
   @inline def apply(x: Repr): Type = x.asInstanceOf[Type]
 }
 
-trait NewTypeApplyM[R] extends NewTypeApply[R] {
+trait NewTypeApplyM extends NewTypeApply {
   /** Convert an `M[Repr]` to a `M[Type]`. */
   @inline def applyM[M[_]](mx: M[Repr]): M[Type] = mx.asInstanceOf[M[Type]]
 }
 
-trait NewTypeDeriving[R] extends NewType[R] {
+trait NewTypeDeriving extends NewType {
   /** Derive an instance of type class `T` if one exists for `Repr`.  */
   def deriving[T[_]](implicit ev: T[Repr]): T[Type] = ev.asInstanceOf[T[Type]]
 }
+
+trait NewTypeExtras
+  extends NewTypeApplyM
+  with NewTypeDeriving
+  with NewTypeAutoOps
--- src/test/scala/io/estatico/newtype/NewTypeTest.scala
+++ src/test/scala/io/estatico/newtype/NewTypeTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class NewTypeTest extends FlatSpec with PropertyChecks with Matchers {
 
   "NewType" should "create a type with no runtime overhead" in {
-    object NatInt extends NewTypeCasts[Int] {
+    object NatInt extends NewType.For[Int] with NewTypeCasts {
       def apply(i: Int): Option[Type] = if (i < 0) None else castM(Some(i))
     }
     NatInt(1) shouldEqual Some(1)
@@ -16,33 +16,37 @@ class NewTypeTest extends FlatSpec with PropertyChecks with Matchers {
 
   it should "find implicit instances" in {
     type Box = Box.Type
-    object Box extends NewTypeDeriving[String] {
+    object Box extends NewType.For[String] with NewTypeDeriving {
       implicit val arb: Arbitrary[Type] = deriving[Arbitrary]
     }
     implicitly[Arbitrary[Box]].arbitrary.sample shouldBe defined
   }
 
   "NewTypeApply" should "automatically create an apply method" in {
-    object PersonId extends NewTypeApply[Int]
+    object PersonId extends NewType.For[Int] with NewTypeApply
     PersonId(1) shouldEqual 1
   }
 
   "DefaultNewType" should "get NewTypeOps" in {
-    object Gold extends DefaultNewType[Double]
+    object Gold extends NewType.Default[Double]
     val gold = Gold(34.56)
     gold.repr shouldEqual 34.56
     gold.withRepr(_ / 2) shouldEqual Gold(17.28)
   }
 
   "NewTypeOps" should "not be available without extending NewTypeAutoOps or importing ops._" in {
-    object Simple extends NewTypeApply[Int]
+    object Simple extends NewType.For[Int] with NewTypeApply
     assertCompiles("Simple(1)")
     assertDoesNotCompile("Simple(1).repr")
     assertCompiles("""
       import io.estatico.newtype.ops._
       Simple(1).repr
     """)
-    object HasOps extends NewTypeApply[Int] with NewTypeAutoOps[Int]
+    object HasOps extends NewType.For[Int] with NewTypeApply with NewTypeAutoOps
     assertCompiles("HasOps(1).repr")
+    assertCompiles("""
+      import io.estatico.newtype.ops._
+      HasOps(1).repr
+    """)
   }
 }
